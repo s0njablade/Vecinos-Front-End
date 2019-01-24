804619e6f85d1f6720aa398f821bebcc135ee005
s0njablade <sonjita.chacon@gmail.com>
2019-01-24 14:37:42 -0700

    learning how to create/add tabs

--- lib/pages/community.dart
+++ lib/pages/community.dart
@@ -5,47 +5,55 @@ import './businesses.dart';
 class CommunityPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text('Filter'),
-            ),
-            ListTile(
-              title: Text('Businesses'),
-              onTap: () {
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => BusinessesPage()));
-              },
-              // title: Text('Neighborhood'),
-              // onTap: () {
-              //   Navigator.push(
-              //       context,
-              //       MaterialPageRoute(
-              //           builder: (BuildContext context) =>
-              //           NeighborhoodPage()));
-              // },
-              // title: Text('Category'),
-              // onTap: () {
-              //   Navigator.push(
-              //       context,
-              //       MaterialPageRoute(
-              //           builder: (BuildContext context) =>
-              //           CategoryPage()));
-              // },
-            )
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Filter'),
+              ),
+              ListTile(
+                title: Text('Businesses'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => BusinessesPage()));
+                },
+                // title: Text('Neighborhood'),
+                // onTap: () {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (BuildContext context) =>
+                //           NeighborhoodPage()));
+                // },
+                // title: Text('Category'),
+                // onTap: () {
+                //   Navigator.push(
+                //       context,
+                //       MaterialPageRoute(
+                //           builder: (BuildContext context) =>
+                //           CategoryPage()));
+                // },
+              )
+            ],
+          ),
         ),
-      ),
-      appBar: AppBar(
-        title: Text('Community'),
-      ),
-      body: Center(
-        child: Text('Select Commmunity'),
+        appBar: AppBar(
+          title: Text('Community'),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(text: 'About'),
+            Tab(text: 'Hours'),
+            Tab(text: 'Website'),
+          ]),
+        ),
+        body: Center(
+          child: Text('Select Commmunity'),
+        ),
       ),
     );
     //     body: Center(
