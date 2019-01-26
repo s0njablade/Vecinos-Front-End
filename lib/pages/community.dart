a41345d7551016e5def53abebfa9b2112ee976dc
s0njablade <sonjita.chacon@gmail.com>
2019-01-26 12:14:35 -0700

    creating the search results pages for the community selection

--- lib/pages/community.dart
+++ lib/pages/community.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 import './businesses.dart';
+import './poc.dart';
+import './women.dart';
+
 
 class CommunityPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
       drawer: Drawer(
         child: Column(
@@ -46,14 +49,20 @@ class CommunityPage extends StatelessWidget {
       ),
       appBar: AppBar(
         title: Text('Community'),
-        bottom: TabBar(tabs: <Widget>[
-          Tab(text: 'Women Owned'),
+        bottom: TabBar(
+          tabs: <Widget>[
+          Tab(
+            icon: Icon(Icons.),
+            text: 'Women Owned'),
           Tab(text: 'LGTBQ Owned'),
           Tab(text: 'POC Owned'),
-        ]),
+        ],),
       ),
-      body: Center(
-        child: Text('Select Commmunity'),
+      body: TabBarView(
+        children: <Widget>[
+
+        ],
+      ),
       ),
     ),);
     //     body: Center(
---
+++ lib/pages/women.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class WomenCreatePage extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      
+      return null;
+    }
+}
