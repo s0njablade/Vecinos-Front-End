a1cce4f58b25932ab42239ce98c5c320cb626f7c
s0njablade <sonjita.chacon@gmail.com>
2019-01-27 15:00:27 -0700

    collected the user input and printed them to the debug

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,51 +1,67 @@
 import 'package:flutter/material.dart';
 
-import './businesses.dart';
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
 
-class LoginPage extends StatelessWidget {
+class _LoginPageState extends State<LoginPage> {
+  String _emailValue;
+  String _passwordValue;
+
   @override
   Widget build(BuildContext context) {
-      
-      return  Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text('login'),
         ),
         body: new Container(
-          decoration: new BoxDecoration(
-          image: new DecorationImage(
-          image: new AssetImage('assets/sidewalk.jpg'),
-          colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
-          fit: BoxFit.cover,
-          
-          )
-        ),
-        child: Container(
-        margin: EdgeInsets.all(10.0),
-        child: ListView(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(decoration: InputDecoration(labelText: 'Password'),
-            obscureText: true,
-            ),
-            SizedBox(height: 10.0),
-            RaisedButton(
-            child: Text('Login', style: TextStyle(color: Colors.red[900])),
-            elevation: 8.0,
-            shape: BeveledRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(7.0))
-            ),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              Navigator.pushReplacementNamed(context, '/businesses');
-              },
-            )
-          ],
-        )
-        )
-        )
-      );
-    }
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
+              image: new AssetImage('assets/sidewalk.jpg'),
+              colorFilter: new ColorFilter.mode(
+                  Colors.black.withOpacity(0.5), BlendMode.dstATop),
+              fit: BoxFit.cover,
+            )),
+            child: Container(
+                margin: EdgeInsets.all(10.0),
+                child: ListView(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (String value) {
+                        setState(() {
+                          _emailValue = value;
+                        });
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      onChanged: (String value) {
+                        setState(() {
+                          _passwordValue = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      child: Text('Login',
+                          style: TextStyle(color: Colors.red[900])),
+                      elevation: 8.0,
+                      shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(7.0))),
+                      color: Theme.of(context).accentColor,
+                      onPressed: () {
+                        print(_emailValue);
+                        print(_passwordValue);
+                        Navigator.pushReplacementNamed(context, '/businesses');
+                      },
+                    )
+                  ],
+                ))));
+  }
 }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
-import './businesses.dart';
+class SignupPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SignupPage();
+  }
+}
 
-class SignupPage extends StatelessWidget {
+class _SignupPage extends State<SignupPage> {
+  String _nameValue;
+  String _emailValue;
+  String _passwordValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,46 +19,60 @@ class SignupPage extends StatelessWidget {
           title: Text('SIGN UP WITH EMAIL'),
         ),
         body: new Container(
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
               image: new AssetImage('assets/sidewalk.jpg'),
-              colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+              colorFilter: new ColorFilter.mode(
+                  Colors.black.withOpacity(0.5), BlendMode.dstATop),
               fit: BoxFit.cover,
-            )
-          ),
-
-          child: Container(
-            margin: EdgeInsets.all(10.0),
-            child: ListView(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(labelText: 'First Name'),
-                  keyboardType: TextInputType.text,
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Email'),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                ),
-                SizedBox(height: 10.0),
-                RaisedButton(
-                  child:
-                      Text('Create Account', style: TextStyle(color: Colors.red[900])),
-                  elevation: 8.0,
-                  shape: BeveledRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(7.0))),
-                  color: Theme.of(context).accentColor,
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/businesses');
-                  },
-                )
-              ],
-            )
-            )
-            )
-          );
+            )),
+            child: Container(
+                margin: EdgeInsets.all(10.0),
+                child: ListView(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Name'),
+                      keyboardType: TextInputType.text,
+                      onChanged: (String value) {
+                        setState(() {
+                          _nameValue = value;
+                        });
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (String value) {
+                        setState(() {
+                          _emailValue = value;
+                        });
+                      },
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      onChanged: (String value) {
+                        setState(() {
+                          _passwordValue = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      child: Text('Create Account',
+                          style: TextStyle(color: Colors.red[900])),
+                      elevation: 8.0,
+                      shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(7.0))),
+                      color: Theme.of(context).accentColor,
+                      onPressed: () {
+                        print(_nameValue);
+                        print(_emailValue);
+                        print(_passwordValue);
+                        Navigator.pushReplacementNamed(context, '/businesses');
+                      },
+                    )
+                  ],
+                ))));
   }
 }
