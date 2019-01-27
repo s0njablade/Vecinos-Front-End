de71d911037c80c8cafa3d10a9181877a8af9c90
s0njablade <sonjita.chacon@gmail.com>
2019-01-26 18:15:53 -0700

    playing with styling: added logo to auth page, a background color and shape of button

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -5,22 +5,43 @@ import './businesses.dart';
 class AuthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Login'),
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (BuildContext context) => BusinessesPage()),
-            );
-          },
-        ),
-      ),
-    );
+    return new Container(
+        // appBar: AppBar(
+        //   title: Text('Login'),
+        // ),
+        decoration: new BoxDecoration(color: Colors.teal[200]),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/logo.png',
+                width: 350.0,
+                height: 600.0,
+              ),
+              // Container(
+              //   padding: EdgeInsets.all(5.0),
+              // ),
+              Container(
+                // padding: EdgeInsets.all(1.0),
+                child: RaisedButton(
+                  child: Text('Login'),
+                  elevation: 8.0,
+                  shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(7.0))
+                  ),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => BusinessesPage()),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
